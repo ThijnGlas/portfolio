f79c8ee7d76097b6ff11456d27f7b6a7c79df286
Thijn <thijnglas@gmail.com>
2022-09-08 13:59:49 +0200

    homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,23 @@
 <x-site-layout>
     <x-slot name="pagetitle">
-
     </x-slot>
-
+    <div id="page">
+    <div id="blokken" class="">
+        <a href="/overmij">
+            <div id="blok1" class="bg-lightblue text-white m-5">
+            <h2>over mij</h2>
+        </div></a>
+        <a href="/blog">
+        <div id="blok2" class="bg-lightblue text-white m-5">
+            <h2>blog</h2>
+        </div></a>
+        <div id="blok3" class="bg-lightblue text-white m-5">
+            <h2>projects</h2>
+        </div>
+        <a href="/contact">
+        <div id="blok4" class="bg-lightblue text-white m-5">
+            <h2>contact</h2>
+        </div></a>
+    </div>
+    </div>
 </x-site-layout>
